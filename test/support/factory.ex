02f35f89432d0c0b853956e9ebb165c455215aaf
parzivalClaus <claudio@kaizenfotoprodutos.com.br>
2021-03-29 14:55:37 -0300

    testing item struct

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
+  alias Exlivery.Orders.Item
   alias Exlivery.Users.User
 
   def user_factory do
@@ -10,6 +11,15 @@ defmodule Exlivery.Factory do
       cpf: "39949445892",
       age: 29,
       address: "Rua Clemente Falcão, 383"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      description: "Pizza de peperoni",
+      category: :pizza,
+      quantity: 1,
+      unity_price: Decimal.new("35.5")
     }
   end
 end
